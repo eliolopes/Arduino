e679fd5e9e3f5b05c75b69939ba05687930c6998
eliolopes <eliolopes@gmail.com>
2015-04-27 22:30:42 +0100

    Create ManualMechanicalSiren.ino

---
+++ ManualMechanicalSiren.ino
@@ -0,0 +1,41 @@
+const int button = A5;
+const int speaker = 9;
+
+const int lower = 100;
+const int higher = 3000;
+
+const float increment = 1;
+const float decrement = 0.0005;
+
+float frequency = lower;
+
+void setup() { 
+  pinMode(button, INPUT_PULLUP);
+  pinMode(speaker, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(button) == LOW and frequency < higher) {
+    increaseFrequency();
+  }
+  else if (frequency > lower) {
+    decreaseFrequency();
+  }
+
+  if (frequency > lower) {
+    tone(speaker, frequency);
+  }
+  else {
+    noTone(speaker);
+  }
+
+  delay(1);
+}
+
+void increaseFrequency() {
+  frequency += 1;
+}
+
+void decreaseFrequency() {
+  frequency = frequency * (1 - decrement);
+}
